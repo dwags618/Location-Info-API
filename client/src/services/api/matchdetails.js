d13fae68b15e6012b39983fa4e8a119d16230d72
dwags618 <dwags618@gmail.com>
2018-06-06 09:42:50 -0700

    removed Auth

--- client/src/services/api/matchdetails.js
+++ client/src/services/api/matchdetails.js
@@ -1,21 +1,3 @@
-import Auth from '../../services/auth';
-
-export const saveBetAmount = (params) => {
-  return fetch('/api/matchdetails', {
-    method: 'PUT',
-    headers: {'Content-Type':'application/json'},
-    body: JSON.stringify(params)
-  });
-}
-
-export const saveImage = (params) => {
-  return fetch('/api/matchdetails/image', {
-    method: 'PUT',
-    headers: {'Content-Type':'application/json'},
-    body: JSON.stringify(params)
-  });
-}
-
 export const getElevation = () => {
   return fetch('/api/matchdetails');
 }
